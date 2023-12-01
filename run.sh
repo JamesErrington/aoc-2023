75e309ff198e9998749fe37259701d122bbd42f2
JamesErrington <james.errington@protonmail.com>
2023-12-01 13:00:38 +0000

    Setup

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ] ; then
+    echo "Usage: ./run.sh <number>"
+    exit 1
+fi
+
+src=$1/main.c
+out=build/$1
+exec=$out/main
+
+mkdir -p $out
+
+clang -Wall -Wextra -O0 -o $exec $src
+
+chmod +x $exec
+
+./$exec
